06df9a312db07e279291b0bd92c61378f6e97b67
S542046 <77809588+S542046@users.noreply.github.com>
2021-03-24 17:48:17 -0500

    last commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var sudh=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  sudh = sudh+1;
+  res.send('User accesses are: '+sudh);
 });
 
 module.exports = router;
